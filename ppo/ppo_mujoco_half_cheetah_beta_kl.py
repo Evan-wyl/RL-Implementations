ab267c925b1a2b286caa0f8dd77b4ebd21ae47e9
liwanye <evan.wy.li@gmail.com>
2023-10-05 20:51:56 +0800

    modify content of ppo_mujoco_half_cheetah_beta_kl file v5

--- ppo/ppo_mujoco_half_cheetah_beta_kl.py
+++ ppo/ppo_mujoco_half_cheetah_beta_kl.py
@@ -366,7 +366,7 @@ if __name__ == '__main__':
                         kl_ent_coef  = kl_ent_coef / 2
                     elif approx_kl > args.target_kl * 1.5:
                         kl_ent_coef = kl_ent_coef * 2
-                    loss = pg_loss - kl_ent_coef * approx_kl + v_loss * args.vf_coef
+                    loss = pg_loss1 - kl_ent_coef * approx_kl + v_loss * args.vf_coef
 
                     logging.info("calculating gradient")
                     optimizer.zero_grad()
